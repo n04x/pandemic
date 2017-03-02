2db35d17fcd596d8fa97c214b9e9facef582d617
Julian Hoang <ju_hoang@encs.concordia.ca>
2017-03-02 15:07:30 -0500

    Implement show_deck command
    Closes #28

--- controller/view/show_deck.cpp
+++ controller/view/show_deck.cpp
@@ -15,7 +15,7 @@ auto show_deck::run(context &ctx, args_t const &args, ostream_t &out) const -> v
         static constexpr auto col2 = 8;
         static constexpr auto fill = ' ';
         out << std::left << std::setw(col1) << std::setfill(fill) << "DECK";
-        out << std::left << std::setw(col2) << std::setfill(fill) << "CARDS";
+        out << std::left << std::setw(col2) << std::setfill(fill) << "SIZE";
         out << std::endl;
         for (auto const &deck : ctx.decks) {
             auto name = deck.first;
@@ -24,5 +24,15 @@ auto show_deck::run(context &ctx, args_t const &args, ostream_t &out) const -> v
             out << std::left << std::setw(col2) << std::setfill(fill) << card_count;
             out << std::endl;
         }
+        return;
+    }
+    auto const &deck = args.at(0);
+    try {
+        for (auto i = ctx.decks.begin(deck); i != ctx.decks.end(deck); i++) {
+            out << *i;
+        }
+        out << std::endl;
+    } catch (std::out_of_range const &) {
+        out << "show-deck: '" << deck << "' does not exist" << std::endl;
     }
 }
