96aaefb2f1c1aaf0bd0835306e5f1708edf29001
n04x <ThomasBacks@gmail.com>
2017-03-08 19:42:35 -0500

    Implement add-cities-to-deck command
    Closes #3

---
+++ controller/editor/add_cities_to_deck.cpp
@@ -0,0 +1,21 @@
+#include "add_cities_to_deck.h"
+
+auto add_cities_to_deck::name() const -> std::string {
+	return "add-cities-to-deck";
+}
+
+auto add_cities_to_deck::description() const -> std::string {
+	return "Add a cities to the deck";
+}
+
+auto add_cities_to_deck::run(context &ctx, args_t const &args, ostream_t &out) const -> void {
+	try {
+		auto name = args.at(0);
+		for (auto city : ctx.cities) {
+			ctx.decks.add_to_top(name, city.first);
+		}
+	}
+	catch (std::out_of_range const &) {
+		out << "usage: " << name() << "<deck>" << std::endl;
+	}
+}
