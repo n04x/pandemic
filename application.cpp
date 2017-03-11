29cb351f7de8f2be30c49ed98e48e8e11e719f15
Markisino <tropiano.m@gmail.com>
2017-03-11 14:48:59 -0500

    Implement drive-to
    Closes #13

--- application.cpp
+++ application.cpp
@@ -22,6 +22,7 @@
 #include "controller/editor/infect.h"
 #include "controller/action/direct_flight_to.h"
 #include "controller/draw_turn.h"
+#include "controller/action/drive_to.h"
 #include <iomanip>
 #include <sstream>
 #include <fstream>
@@ -57,6 +58,7 @@ application::application(std::istream &in, std::ostream &out) :
 	insert_controller<set_disease_cube_supply>();
 	insert_controller<infect>();
 	insert_controller<direct_flight_to>();
+	insert_controller<drive_to>();
 };
 
 auto application::help() -> void {
---
+++ controller/action/drive_to.cpp
@@ -0,0 +1,39 @@
+#include "drive_to.h"
+#include <iostream>
+
+using namespace std;
+
+auto drive_to::name() const -> std::string
+{
+	return "drive-to";
+}
+
+auto drive_to::description() const -> std::string
+{
+	return "Drive to city selected";
+}
+
+auto drive_to::run(context & ctx, args_t const & args, ostream_t & out) const -> void
+{
+	try {
+		auto city = args.at(0);
+		auto player = ctx.players.get_current_turn();	// Get the information of which player is playing
+		auto currentCity = ctx.players.get_city(player);	// City player is currently on
+
+		bool connected = false;
+
+		for (auto connection = ctx.cities.begin(currentCity); connection != ctx.cities.end(currentCity); connection++) {
+			if (*connection == city)
+				connected = true;
+		}
+
+		if (connected == true)
+			ctx.players.set_city(player, city);				// Set the new position of player
+
+		else
+			out << "Not possible to drive to " << city << endl;
+	}
+	catch (std::out_of_range const &) {
+		out << "usage: " << name() << " <city>" << std::endl;
+	}
+}
