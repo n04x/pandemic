ac68f04bbcc454ab44022852e3d53b63f1267a9b
Julian Hoang <ju_hoang@encs.concordia.ca>
2017-03-12 16:24:56 -0400

    Fix direct-flight-to
    #14

--- controller/action/direct_flight_to.cpp
+++ controller/action/direct_flight_to.cpp
@@ -1,7 +1,7 @@
 #include "direct_flight_to.h"
 
 auto direct_flight_to::name() const->std::string {
-	return "direct-flight-to...";
+	return "direct-flight-to";
 }
 
 auto direct_flight_to::description() const->std::string {
@@ -13,6 +13,7 @@ auto direct_flight_to::run(context &ctx, args_t const &args, ostream_t &out) con
 		auto city = args.at(0);
 		auto deck = args.at(1);
 		auto player = ctx.players.get_current_turn();	// Get the information of which player is playing
+		ctx.decks.remove(player, city);
 		ctx.decks.add_to_top(deck, city);				// Add the card to the discard card deck
 		ctx.players.set_city(player, city);				// Set the new position of player
 		ctx.players.decrement_actions_remaining();
