87f5bb1d1b19cb0be9aa39b7ed58477d16d8f71d
Julian Hoang <ju_hoang@encs.concordia.ca>
2017-03-13 18:24:18 -0400

    Add more information to status

--- view/status.cpp
+++ view/status.cpp
@@ -18,11 +18,39 @@ auto status::run(context &ctx, args_type const &args, ostream_type &out) const -
 	}
 	static constexpr auto col1 = 5;
 	static constexpr auto col2 = 8;
+	static constexpr auto col3 = 12;
 	static constexpr auto fill = ' ';
 	out << std::left << std::setw(col1) << std::setfill(fill) << "TURN";
-	out << std::left << std::setw(col2) << std::setfill(fill) << "REMAINING";
+	out << std::left << std::setw(col2) << std::setfill(fill) << "ACTIONS";
+	out << std::left << std::setw(col2) << std::setfill(fill) << "BLACK";
+	out << std::left << std::setw(col2) << std::setfill(fill) << "YELLOW";
+	out << std::left << std::setw(col2) << std::setfill(fill) << "BLUE";
+	out << std::left << std::setw(col2) << std::setfill(fill) << "RED";
+	out << std::left << std::setw(col3) << std::setfill(fill) << "RESEARCH";
+	out << std::left << std::setw(col3) << std::setfill(fill) << "OUTBREAK";
+	out << std::left << std::setw(col3) << std::setfill(fill) << "INFECTION";
+	out << std::left << std::setw(col2) << std::setfill(fill) << "CURES";
 	out << std::endl;
 	out << std::left << std::setw(col1) << std::setfill(fill) << ctx.players.get_current_turn();
 	out << std::left << std::setw(col2) << std::setfill(fill) << ctx.players.get_actions_remaining();
+	out << std::left << std::setw(col2) << std::setfill(fill) << ctx.game.cube_supply_count("black"_h);
+	out << std::left << std::setw(col2) << std::setfill(fill) << ctx.game.cube_supply_count("yellow"_h);
+	out << std::left << std::setw(col2) << std::setfill(fill) << ctx.game.cube_supply_count("blue"_h);
+	out << std::left << std::setw(col2) << std::setfill(fill) << ctx.game.cube_supply_count("red"_h);
+	out << std::left << std::setw(col3) << std::setfill(fill) << ctx.game.research_station_supply_count();
+	out << std::left << std::setw(col3) << std::setfill(fill) << ctx.game.get_outbreak_level();
+	out << std::left << std::setw(col3) << std::setfill(fill) << ctx.game.get_infection_rate();
+	if (ctx.game.discovered_cure("black"_h)) {
+		out << "black ";
+	}
+	if (ctx.game.discovered_cure("yellow"_h)) {
+		out << "yellow ";
+	}
+	if (ctx.game.discovered_cure("red"_h)) {
+		out << "red ";
+	}
+	if (ctx.game.discovered_cure("blue"_h)) {
+		out << "blue ";
+	}
 	out << std::endl;
 }
