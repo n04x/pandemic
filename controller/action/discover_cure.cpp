1b18b6b9c0c671bfd6cfc5ee60737409f3ef9169
n04x <ThomasBacks@gmail.com>
2017-04-09 20:16:50 -0400

    Scientist Role Condition
    Add If Statement in the discover_cure.cpp
    Closes #38

--- controller/action/discover_cure.cpp
+++ controller/action/discover_cure.cpp
@@ -10,9 +10,11 @@ auto discover_cure::description() const -> std::string {
 
 auto discover_cure::run(context &ctx, args_type const &args, ostream_type &out) const -> void {
 	auto discardDeck = "player_discard"_h;
+	auto roleCheck = "Scientist"_h;
 	try {
 		auto color = args.at(0);
 		auto player = ctx.players.get_current_turn();
+		auto role = ctx.players.get_role(player);
 		if (args.size() > 1) {
 			discardDeck = args.at(1);
 		}
@@ -22,14 +24,28 @@ auto discover_cure::run(context &ctx, args_type const &args, ostream_type &out) 
 			return;
 		}
 
-		std::vector<handle> v;
+		std::vector<handle> v;	
 
 		for (auto card = ctx.decks.begin(player); card != ctx.decks.end(player); card++) {
 			if (ctx.cities.get_color(*card) == color) {
 				v.push_back(*card);
 			}
+			// It will check the role of the user with the role in the condition
+			if (role == roleCheck) {
+				// The Scientist needs only 4 City cards of the same disease color to Discover a Cure for that disease.
+				if (v.size() == 4) {
+					for (int i = 0; i < v.size(); i++) {
+						ctx.decks.remove(player, v[i]);
+						ctx.decks.add_to_top(discardDeck, v[i]);
+					}
+					ctx.game.discovered_cure(color);
+					ctx.players.decrement_actions_remaining();
 
-			if (v.size() == 5) {
+					return;
+				}
+			}
+			// Else if the player is not a scientist.
+			else if (v.size() == 5) {
 				for (int i = 0; i < v.size(); i++) {
 					ctx.decks.remove(player, v[i]);
 					ctx.decks.add_to_top(discardDeck, v[i]);
