d8aaed553c9f95857114fca4592352c2aed1c8ce
n04x <ThomasBacks@gmail.com>
2017-04-11 10:53:17 -0400

    Operation Expert
    Modified the place_research_station action to add an if statement for
    the operation expert role. #35

--- controller/editor/place_research_station.cpp
+++ controller/editor/place_research_station.cpp
@@ -10,10 +10,31 @@ auto place_research_station::description() const -> std::string {
 
 auto place_research_station::run(context &ctx, args_type const &args, ostream_type &out) const -> void {
 	try {
-		auto name = args.at(0);
-		if (ctx.cities.has_research_station(name) == false) {
-			ctx.game.remove_research_station_supply();
-			ctx.cities.place_research_station(name);
+		auto city = args.at(0);								// Renamed for name to city for clarification
+		auto player = ctx.players.get_current_turn();		// Current turn of the player
+		auto role = ctx.players.get_role(player);			// Check the player role.
+
+		if (ctx.cities.has_research_station(city) == false) {
+			// If the player is the Ops Expert AND is in the same city as requested.
+			if (role == "operation_expert"_h && city == ctx.players.get_city(player)) {
+				ctx.game.remove_research_station_supply();
+				ctx.cities.place_research_station(city);
+				ctx.players.decrement_actions_remaining();
+			}
+			else {
+				if (city == ctx.players.get_city(player)) {
+					for (auto card = ctx.decks.begin(player); card != ctx.decks.end(player); card++) {
+						if (*card == city) {
+							ctx.game.remove_research_station_supply();
+							ctx.cities.place_research_station(city);
+							ctx.players.decrement_actions_remaining();
+						}
+					}
+					
+				}
+				
+			}
+			
 		}
 
 	}
