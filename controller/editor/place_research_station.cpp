2f5db30ad4cb552d35fcc0d9e819e2efe79303c7
Markisino <tropiano.m@gmail.com>
2017-04-14 16:36:13 -0400

    Fixed bug #59
    closes #59

--- controller/editor/place_research_station.cpp
+++ controller/editor/place_research_station.cpp
@@ -9,36 +9,55 @@ auto place_research_station::description() const -> std::string {
 }
 
 auto place_research_station::run(context &ctx, args_type const &args, ostream_type &out) const -> void {
-	try {
-		auto city = args.at(0);								// Renamed for name to city for clarification
-		auto player = ctx.players.get_current_turn();		// Current turn of the player
-		auto role = ctx.players.get_role(player);			// Check the player role.
+	
+	if (args.empty()) {
 
+		auto player = ctx.players.get_current_turn();		// Current turn of the player
+		auto city = ctx.players.get_city(player);			// Renamed for name to city for clarification
+		auto role = ctx.players.get_role(player);			// Check the player role.
+		
 		if (ctx.cities.has_research_station(city) == false) {
+			
 			// If the player is the Ops Expert AND is in the same city as requested.
-			if (role == "operation_expert"_h && city == ctx.players.get_city(player)) {
+			if (role == "operation_expert"_h) {
 				ctx.game.remove_research_station_supply();
 				ctx.cities.place_research_station(city);
 				ctx.players.decrement_actions_remaining();
+				return;
 			}
+
 			else {
-				if (city == ctx.players.get_city(player)) {
-					for (auto card = ctx.decks.begin(player); card != ctx.decks.end(player); card++) {
-						if (*card == city) {
-							ctx.game.remove_research_station_supply();
-							ctx.cities.place_research_station(city);
-							ctx.players.decrement_actions_remaining();
-						}
+				for (auto card = ctx.decks.begin(player); card != ctx.decks.end(player); card++) {
+					if (*card == city) {
+						ctx.decks.remove(player, city);
+						ctx.game.remove_research_station_supply();
+						ctx.cities.place_research_station(city);
+						ctx.players.decrement_actions_remaining();
+						return;
 					}
-					
 				}
-				
 			}
-			
+
+			out << "You do not have the " << city << " card in your hand!" << std::endl;
+			return;
 		}
+
+		else {
+			out << city << " already has a reasearch station!" << std::endl;
+			return;
+		}
+	}
+
+	//To be used when setting up board
+	try {
+
+		auto city = args.at(0);								// Renamed for name to city for clarification
+
+		ctx.game.remove_research_station_supply();
+		ctx.cities.place_research_station(city);
 
 	}
 	catch (std::out_of_range const &) {
-		out << "usage: " << name() << "<city>" << std::endl;
+		out << "usage: " << name() << " <city>" << std::endl;
 	}
 }
